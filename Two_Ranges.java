6e777b9ecac90a71f9e10bffff25fe7fd6b17e2f
AviralPatel0526 <aviralpatel2603@gmail.com>
2023-10-05 14:53:44 +0530

    new problem added

---
+++ Two_Ranges.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t>0){
+		    t--;
+		    int count=0;
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    int d=sc.nextInt();
+		  for(int i=1;i<=8;i++){
+		      if(i>=a && i<=b || i>=c && i<=d)
+		      count++;
+		  }
+
+		    
+		  System.out.println(count); 
+		    
+		}
+	}
+}
